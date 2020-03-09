63b215ffe571fbf1122c24809064515d00ed8603
deannabosschert <deanna.bosschert@gmail.com>
2020-03-09 17:57:03 +0100

    remove comments

--- server.js
+++ server.js
@@ -11,16 +11,6 @@ app.get('/', function(req, res) {
   res.redirect('/nerds')
 })
 
-// app.get('/nerds', function(res, json) {
-//   Api.get()
-//   console.log(json)
-//   const data = json
-//   res.render('index', {
-//     nerds: data
-//   })
-// })
-
-
 app.get('/nerds', function(req, res) {
   Api.get().then(data => {
     res.render('index.ejs', {
@@ -29,51 +19,7 @@ app.get('/nerds', function(req, res) {
   })
 })
 
-// //
-// // app.get('/nerds', function(req, res, nerds) {
-// //   Api.get().then(res => {
-// //     // const nerds = JSON.parse(res)
-// //     // console.log(res)
-// //     // const nerds = res
-// //     // console.log(nerds)
-// //     res.render('index.ejs', {
-// //       nerds: res
-// //     })
-//
-//
-//
-//     // console.log(data)
-//     // request(function(error, response, body) {
-//     // const data = JSON.parse(data);
-//     // res.render('index.ejs', {
-//     // nerds: data
-//     // })
-//     // })
-//   })
-// })
 
-
-//
-// const API = {
-//   get: (route) => {
-//     const query = getQuery(route)
-//     const url = `${cors}${endpoint}${query}&authorization=${key}&detaillevel=${detail}&output=json`
-//     fetch(url, config)
-//       .then(res => {
-//         return res.json()
-//       })
-//       .then(json => {
-//         const data = json.results
-//         Data.store(`${route}`, data)
-//       })
-//       .catch(err => {
-//         console.log(err)
-//       })
-//   }
-// }
-//
-
-//
 // app.get('/stories/:id', function(req, res) {
 //   request(host + req.params.id, function(error, response, body) {
 //     const data = JSON.parse(body)
