348e210da8eaa09df0231b7fa4a0224ec76909c1
deannabosschert <deanna.bosschert@gmail.com>
2020-03-09 18:42:42 +0100

    add fetch for detail page

--- modules/api.js
+++ modules/api.js
@@ -1,20 +1,46 @@
 const fetch = require('node-fetch')
 
 const endpoint = "https://api.github.com"
-const minor = "/cmda-minor-web"
 const course = "/web-app-from-scratch-"
 const year = "1920"
-const tag = "/forks"
 const limit = "50"
-const apiLink = `${endpoint}/repos${minor}${course}${year}${tag}?per_page=${limit}`
+const repo = `${course}${year}`
+// const apiLink = `${endpoint}/repos${owner}${repo}?per_page=${limit}`
 
 const Api = {
-  get() {
+  get(route) {
+    const owner = getOwner(route)
+    const tag = getTag(route)
+
+    const apiLink = `${endpoint}/repos/${owner}${repo}${tag}?per_page=${limit}`
+    console.log(apiLink)
     return fetch(apiLink)
       .then(res => res.json())
       .catch(err => {
         console.log(err)
       })
+  }
+}
+
+
+function getOwner(route) {
+  if (route === "overview") {
+    const owner = "cmda-minor-web"
+    return owner
+  } else {
+    const owner = `${route}`
+    return owner
+  }
+}
+
+
+function getTag(route) {
+  if (route === "overview") {
+    const tag = "/forks"
+    return tag
+  } else {
+    const tag = ""
+    return tag
   }
 }
 
