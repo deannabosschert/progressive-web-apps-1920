f76ef1e510350543b8be5dfae3e0a411be729328
deannabosschert <deanna.bosschert@gmail.com>
2020-03-10 13:05:19 +0100

    load data based on route (add 'query'-variable)

--- modules/api.js
+++ modules/api.js
@@ -1,22 +1,21 @@
 const fetch = require('node-fetch')
 
-const endpoint = "https://api.github.com"
-const course = "/web-app-from-scratch-"
-const year = "1920"
+const endpoint = "https://api.github.com/"
+// const course = "/web-app-from-scratch-"
+// const year = "1920"
 const limit = "50"
-const repo = `${course}${year}`
+// const repo = `${course}${year}`
 // const apiLink = `${endpoint}/repos${owner}${repo}?per_page=${limit}`
-
 const Api = {
-  get(route) {
-    const owner = getOwner(route)
-    const tag = getTag(route)
-    const apiLink = `${endpoint}/repos/${owner}${repo}${tag}?per_page=${limit}`
+  get(route, repo) {
+    const query = getQuery(route, repo)
+    const apiLink = `${endpoint}${query}?per_page=${limit}`
     return fetch(apiLink)
       .then(res => res.json())
       .then((json) => {
-        const data = cleanData(json)
-        return data
+        console.log(json)
+        // const data = cleanData(json)
+        return json
       })
       .catch(err => {
         console.log(err)
@@ -24,74 +23,98 @@ const Api = {
   }
 }
 
+//
+// function getOwner(route) {
+//   if (route === "overview") {
+//     const owner = "cmda-minor-web"
+//     return owner
+//   } else if (route === "/") {
+//     const owner = "cmda-minor-web"
+//     return owner
+//   } else {
+//     const owner = `${route}`
+//     return owner
+//   }
+// }
 
-function getOwner(route) {
-  if (route === "overview") {
-    const owner = "cmda-minor-web"
-    return owner
+function getQuery(route, repo) {
+  if (route === "home") {
+    const query = "users/cmda-minor-web/repos"
+    return query
+  }
+  if (route === "course") {
+    const query = `/repos/cmda-minor-web/${repo}/forks`
+    return query
   } else {
-    const owner = `${route}`
-    return owner
+    const query = `/repos/${route}/${repo}`
+    // GET / repos /: owner /: repo
+
+    return query
   }
 }
 
+//
+// function getTag(route) {
+//   if (route === "overview") {
+//     const tag = "/forks"
+//     return tag
+//   } else if (route === "/") {
+//     const owner = ""
+//     return owner
+//   } else {
+//     const tag = ""
+//     return tag
+//   }
+// }
 
-function getTag(route) {
-  if (route === "overview") {
-    const tag = "/forks"
-    return tag
-  } else {
-    const tag = ""
-    return tag
-  }
+
+
+function cleanData(results) {
+  console.log("test")
+  const data = results.map(result => ({
+    courseName: result.name,
+    name: result.owner.login,
+    description: result.description,
+    avatar: result.owner.avatar_url,
+    homepage: result.homepage,
+    id: result.id,
+    node_id: result.node_id,
+    full_name: result.full_name,
+    private: result.private,
+    owner: result.owner,
+    html_url: result.html_url,
+    milestones_url: result.milestones_url,
+    notifications_url: result.notifications_url,
+    labels_url: result.labels_url,
+    releases_url: result.releases_url,
+    deployments_url: result.deployments_url,
+    created_at: result.created_at,
+    updated_at: result.updated_at,
+    pushed_at: result.pushed_at,
+    git_url: result.git_url,
+    ssh_url: result.ssh_url,
+    clone_url: result.clone_url,
+    svn_url: result.svn_url,
+    homepage: result.homepage,
+    size: result.size,
+    stargazers_count: result.stargazers_count,
+    watchers_count: result.watchers_count,
+    language: result.language,
+    has_issues: result.has_issues,
+    has_projects: result.has_projects,
+    has_downloads: result.has_downloads,
+    has_wiki: result.has_wiki,
+    has_pages: result.has_pages,
+    forks_count: result.forks_count,
+    mirror_url: result.mirror_url,
+    archived: result.archived,
+    disabled: result.disabled,
+    open_issues_count: result.open_issues_count
+  }))
+
+  return data
 }
 
-
-function cleanData(data) {
-  return data.map(nerd => {
-    return {
-      courseName: nerd.name,
-      name: nerd.owner.login,
-      description: nerd.description,
-      avatar: nerd.owner.avatar_url,
-      homepage: nerd.homepage,
-      id: nerd.id,
-      node_id: nerd.node_id,
-      name: nerd.name,
-      full_name: nerd.full_name,
-      private: nerd.private,
-      owner: nerd.owner,
-      html_url: nerd.html_url,
-      milestones_url: nerd.milestones_url,
-      notifications_url: nerd.notifications_url,
-      labels_url: nerd.labels_url,
-      releases_url: nerd.releases_url,
-      deployments_url: nerd.deployments_url,
-      created_at: nerd.created_at,
-      updated_at: nerd.updated_at,
-      pushed_at: nerd.pushed_at,
-      git_url: nerd.git_url,
-      ssh_url: nerd.ssh_url,
-      clone_url: nerd.clone_url,
-      svn_url: nerd.svn_url,
-      homepage: nerd.homepage,
-      size: nerd.size,
-      stargazers_count: nerd.stargazers_count,
-      watchers_count: nerd.watchers_count,
-      language: nerd.language,
-      has_issues: nerd.has_issues,
-      has_projects: nerd.has_projects,
-      has_downloads: nerd.has_downloads,
-      has_wiki: nerd.has_wiki,
-      has_pages: nerd.has_pages,
-      forks_count: nerd.forks_count,
-      mirror_url: nerd.mirror_url,
-      archived: nerd.archived,
-      disabled: nerd.disabled,
-      open_issues_count: nerd.open_issues_count
-    }
-  })
-}
 
 
 
